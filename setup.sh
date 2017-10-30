ed146526a671aed6b265cc1e84c9ccca2d0d5e29
jontoto <jontoto@github.com>
2017-10-30 15:37:06 -0400

    finderbot with lidar now

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 path_dst=~/.gazebo/models/
 path_lidar_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/hokuyo_utm30lx
 path_robot_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/finderbot_model
+path_complete_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/Finderbot_Lidar
 cp -r $path_lidar_src $path_dst
 cp -r $path_robot_src $path_dst
+cp -r $path_complete_src $path_dst
