3d5320c9ce541c84491ff352e09448db5e6961a6
jontoto <jontoto@github.com>
2017-11-02 13:12:29 -0400

    add imu to finderbot some name cahnges

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 path_dst=~/.gazebo/models/
-path_lidar_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/hokuyo_utm30lx
-path_robot_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/finderbot_model
-path_complete_src=~/catkin_ws/src/finderbot/FinderBot_Gazebo/Finderbot_Lidar
+path_src_dir=~/catkin_ws/src/finderbot/FinderBot_Gazebo/
+path_lidar_src=${path_src_dir}/hokuyo_utm30lx
+path_robot_src=${path_src_dir}/finderbot_model
+path_complete_src=${path_src_dir}/Finderbot_Lidar
+path_imu_src=${path_src_dir}/noisy_imu
 cp -r $path_lidar_src $path_dst
 cp -r $path_robot_src $path_dst
 cp -r $path_complete_src $path_dst
+cp -r $path_imu_src $path_dst
