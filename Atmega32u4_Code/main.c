01cc6178f1ec89eab323916fdb7a718f90627c98
Melinda Kothbauer <mkothbau@umich.edu>
2017-11-24 15:45:05 -0500

    two pwm signals working, trying to sync up frequencies

--- Atmega32u4_Code/main.c
+++ Atmega32u4_Code/main.c
@@ -12,6 +12,8 @@ void TaskSendSPIData(void *pvParameters);
 
 void TaskPIDController(void *pvParameters);
 
+void TaskTestTimers(void *pvParameters);
+
 /* speeds updated by PID and MotorCommand */
 int speedFL = 0;
 int speedFR = 0;
@@ -58,52 +60,87 @@ unsigned char inByte;
 char message[MESSAGE_MAX_SIZE];
 char command;
 
+#define F_CPU 16000000
+
 int main(void)
 {
+
     xTaskCreate(
-    TaskMotorCommand
-    ,  (const portCHAR *)"MotorCommand"  // A name just for humans
+    TaskTestTimers
+    ,  (const portCHAR *)"TimerTest"  // A name just for humans
     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  5  // Priority (low num = low priority)
     ,  NULL );
 
-    xTaskCreate(
-    TaskIRSensorRead
-    ,  (const portCHAR *)"IRSEnsorRead"  // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  3  // Priority (low num = low priority)
-    ,  NULL );
+    // xTaskCreate(
+    // TaskMotorCommand
+    // ,  (const portCHAR *)"MotorCommand"  // A name just for humans
+    // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    // ,  NULL
+    // ,  5  // Priority (low num = low priority)
+    // ,  NULL );
 
-    xTaskCreate(
-    TaskIMURead
-    ,  (const portCHAR *)"IMURead"  // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority (low num = low priority)
-    ,  NULL );
+    // xTaskCreate(
+    // TaskIRSensorRead
+    // ,  (const portCHAR *)"IRSensorRead"  // A name just for humans
+    // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    // ,  NULL
+    // ,  3  // Priority (low num = low priority)
+    // ,  NULL );
 
-    xTaskCreate(
-    TaskSendSPIData
-    ,  (const portCHAR *)"SendSPIData"  // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority (low num = low priority)
-    ,  NULL );
+    // xTaskCreate(
+    // TaskIMURead
+    // ,  (const portCHAR *)"IMURead"  // A name just for humans
+    // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    // ,  NULL
+    // ,  2  // Priority (low num = low priority)
+    // ,  NULL );
 
-    xTaskCreate(
-    TaskPIDController
-    ,  (const portCHAR *)"PIDController"  // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  1  // Priority (low num = low priority)
-    ,  NULL );
+    // xTaskCreate(
+    // TaskSendSPIData
+    // ,  (const portCHAR *)"SendSPIData"  // A name just for humans
+    // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    // ,  NULL
+    // ,  2  // Priority (low num = low priority)
+    // ,  NULL );
+
+    // xTaskCreate(
+    // TaskPIDController
+    // ,  (const portCHAR *)"PIDController"  // A name just for humans
+    // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    // ,  NULL
+    // ,  1  // Priority (low num = low priority)
+    // ,  NULL );
 
 
     vTaskStartScheduler();
 
     return 0;               /* never reached */
+}
+
+void TaskTestTimers(void *pvParameters) {
+    
+    DDRC |= _BV(PC7) | _BV(PC6); //set DDC7 = 1 -> PC7 is output pin, DDC6 = 1 ->PC6 is output
+
+    DDRD = 1;           /* make the D0 pin an output */   
+
+    
+    PORTC = 0x00;
+
+    TCCR4A |= _BV(PWM4A) | _BV(WGM40) | _BV(COM4A1); //initiallize Timer 4 in freq/phase correct PWM mode
+    TCCR4B = _BV(CS40); //initialize counter 4 with no prescaler
+
+    TCCR3A |= _BV(WGM30) | _BV(COM3A1); //initiallize Timer 3 in 8 bit freq/phase correct PWM mode
+    TCCR3B = _BV(CS31) ;//| _BV(CS30); //initialize counter 3 with prescaler 
+    
+    for(;;)
+    {
+        OCR3A = 0x7F;
+        OCR4A = 0x7F; //set duty cycle into compare reg. TOP is 0xFF by default. 
+        vTaskDelay(pdMS_TO_TICKS(100));
+        PORTD ^= 1;    /* toggle the LED */
+    }
 }
 
 void TaskMotorCommand( void *pvParameters)  // This is a Task.
@@ -114,4 +151,20 @@ void TaskMotorCommand( void *pvParameters)  // This is a Task.
         vTaskDelay(pdMS_TO_TICKS(100));
 	    PORTD ^= 1;    /* toggle the LED */
     }
+}
+
+void TaskIRSensorRead(void *pvParameters) {
+
+}
+
+void TaskIMURead(void *pvParameters) {
+
+}
+
+void TaskSendSPIData(void *pvParameters) {
+
+}
+
+void TaskPIDController(void *pvParameters) {
+
 }
