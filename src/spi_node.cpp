aa7725a8f2b318b9efcf212b4813077d94fb74e9
jontoto <jontoto@github.com>
2017-12-06 01:42:52 -0500

    heading and motor command

--- src/spi_node.cpp
+++ src/spi_node.cpp
@@ -93,19 +93,180 @@ float readWriteAccelPCB(unsigned char* buf)
     return bytes_to_float.data;
 }
 
-void printBuf(unsigned char* buf)
+void printBuf(unsigned char* buf, int size)
 {
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < size; i++)
     {
         ROS_INFO("SPI: %c  %i", buf[i], (uint8_t)buf[i]);
     }
 }
 
 
+void readWriteHeading(float heading)
+{
+    int channel = 0;
 
+    float_bytes bytes_to_float;
+    bytes_to_float.data = heading;
+
+    unsigned char buf[9] = "sH0000e_";
+
+    buf[2] = bytes_to_float.bytes.zero;
+    buf[3] = bytes_to_float.bytes.one;
+    buf[4] = bytes_to_float.bytes.two;
+    buf[5] = bytes_to_float.bytes.three;
+    
+    //send s
+    int ret = wiringPiSPIDataRW (channel, buf, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+
+    //send C
+    ret = wiringPiSPIDataRW(channel, buf+1, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    //check that start bit returned ack
+    if(buf[1] != '!') ROS_ERROR("No Ack to start");
+
+    //send data1
+    ret = wiringPiSPIDataRW(channel, buf+2, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[2] != '!')
+    {
+        ROS_ERROR("No Ack to H");
+    }
+
+    //send data 2
+    ret = wiringPiSPIDataRW(channel, buf+3, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if((uint8_t)buf[3] != bytes_to_float.bytes.zero)
+    {
+        ROS_ERROR("Incorrect byte 1");
+    }
+
+    //send data 3
+    ret = wiringPiSPIDataRW(channel, buf+4, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[4] != bytes_to_float.bytes.one)
+    {
+        ROS_ERROR("Incorrect byte 2");
+    }
+
+    //send data 4
+    ret = wiringPiSPIDataRW(channel, buf+5, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[5] != bytes_to_float.bytes.two)
+    {
+        ROS_ERROR("Incorrect byte 3");
+    }
+
+    //send end
+    ret = wiringPiSPIDataRW(channel, buf+6, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[6] != bytes_to_float.bytes.three)
+    {
+        ROS_ERROR("Incorrect byte 4");
+    }
+
+    //send _
+    ret = wiringPiSPIDataRW(channel, buf+7, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[7] != 'd')
+    {
+        ROS_ERROR("Incorrect byte 4");
+    }
+
+}
+
+
+
+void readWriteMotor(unsigned char *buf)
+{
+    int channel = 0;
+    
+    //send start bit
+    int ret = wiringPiSPIDataRW (channel, buf, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+
+    //send C
+    ret = wiringPiSPIDataRW(channel, buf+1, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    //check that start bit returned ack
+    if(buf[1] != '!') ROS_ERROR("No Ack to start");
+
+    //send fBhLR
+    unsigned char cmd = buf[2];
+    ret = wiringPiSPIDataRW(channel, buf+2, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[2] != '!')
+    {
+        ROS_ERROR("No Ack to C");
+    }
+
+    //send end byte
+    ret = wiringPiSPIDataRW(channel, buf+3, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[3] != cmd) ROS_ERROR("SPI: sent: %c, got %c back", cmd, buf[3]); 
+
+    //send empty
+    ret = wiringPiSPIDataRW(channel, buf+4, 1);
+    if (ret < 0)
+    {
+        ROS_ERROR("SPI: ret = %i\n", ret);
+        exit(1);
+    }
+    if(buf[4] != 'd') ROS_ERROR("No end ACK");
+
+}
 int main(int argc, char** argv)
 {   
-    ros::init(argc, argv, "SPI Transmitter");
+    ros::init(argc, argv, "SPI_Transmitter");
     ros::NodeHandle nh;
 
     int channel = 0;
@@ -119,16 +280,16 @@ int main(int argc, char** argv)
         unsigned char buf[9] = "sX1234e_";
 
         ROS_INFO("lin accel(x) = %f", readWriteAccelPCB(buf));
+        printBuf(buf, 9);
+        // memcpy(buf,"sy1234e_", 9);
 
-        memcpy(buf,"sy1234e_", 9);
+        // ROS_INFO("lin accel(x) = %f", readWriteAccelPCB(buf));
 
-        ROS_INFO("lin accel(x) = %f", readWriteAccelPCB(buf));
+        // memcpy(buf,"sT1234e_", 9);
 
-        memcpy(buf,"sT1234e_", 9);
+        // ROS_INFO("lin accel(x) = %f", readWriteAccelPCB(buf));        
 
-        ROS_INFO("lin accel(x) = %f", readWriteAccelPCB(buf));        
-
-        loop_rate.sleep();   
+        // loop_rate.sleep();   
     }
 
     return 0;
