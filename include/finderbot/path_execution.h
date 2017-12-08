079614f59183d5ed3a4de0cbc76a2c327b687291
jhmitchel <jhmitchel@gmail.com>
2017-12-08 14:24:34 -0500

    repo without the UWB code addeed

--- include/finderbot/path_execution.h
+++ include/finderbot/path_execution.h
@@ -7,12 +7,12 @@
 #include <finderbot/map_utils.h>
 #include <finderbot/Pose.h>
 #include <geometry_msgs/Twist.h>
-#include <finderbot/uwb.h>
-#include <finderbot/UWB/ModuleConnector.hpp>
+// #include <finderbot/uwb.h>
+// #include <finderbot/UWB/ModuleConnector.hpp>
 #include <ctime>
 #include <iostream>
 #include <unistd.h>
-#include <finderbot/UWBScan.h>
+// #include <finderbot/UWBScan.h>
 
 #include <math.h>
 #include <string>
@@ -53,7 +53,7 @@ class Executor
 
   public:
     ros::ServiceClient client_;
-    finderbot::UWBScan srv;
+    // finderbot::UWBScan srv;
     Executor(std::string world_frame, std::string local_frame);
 
     void pathExecution(std::vector<size_t>& path);
