6d2a8ed59f2b3c7df6093002d77aafede44a6fe1
jon <jontoto@umich.edu>
2017-12-09 10:31:54 -0500

    programs to run to just connect

---
+++ wifi_direct/test/connect_base.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include "../wifi_connect.h"
+#include "../data_transmission.h"
+
+int main()
+{
+	// call func for device running on
+	connect_base();
+
+
+    return 0;
+}
+
