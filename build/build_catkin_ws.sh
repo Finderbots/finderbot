3e4d38078545e5f3f405400909601aae632c7901
leerut <leerut@umich.edu>
2017-12-09 18:38:01 -0500

    Not properly getting last line in UWB node

--- build/build_catkin_ws.sh
+++ build/build_catkin_ws.sh
@@ -3,6 +3,6 @@
 echo "Building and all that..."
 cd ~/catkin_ws/
 catkin_make
-sudo chown root:root /path/to/UWB_scan_node/or/desired/folder
-sudo chmod a+rx /path/to/UWB_scan_node/or/desired/folder
-sudo chmod u+s /path/to/UWB_scan_node/or/desired/folder
+sudo chown root:root ~/catkin_ws/devel/lib/finderbot/UWB_scan_node
+sudo chmod a+rx ~/catkin_ws/devel/lib/finderbot/UWB_scan_node
+sudo chmod u+s ~/catkin_ws/devel/lib/finderbot/UWB_scan_node
--- src/uwb_scan_node.cpp
+++ src/uwb_scan_node.cpp
@@ -1,7 +1,5 @@
 #include <ros/ros.h>
 #include <ros/console.h>
-#include <finderbot/uwb.h>
-#include <finderbot/UWB/ModuleConnector.hpp>
 // #include <finderbot/path_execution_node.h>
 #include <ctime>
 #include <iostream>
@@ -13,13 +11,12 @@
 #include <finderbot/UWB_data.h>
 #include <finderbot/servo.h>
 
-ros::Publisher uwb_publisher;
-ros::NodeHandle nh;
 finderbot::UWB_data uwb_data;
+ros::Publisher uwb_publisher;
 
-string getLastLine(fstream file) {
+std::string getLastLine(std::fstream &file) {
 	bool keep_looping = true;
-	file.seekg();
+	file.seekg(-1,std::ios_base::end);
 	while (!keep_looping) {
 		char ch;
 		file.get(ch);
@@ -27,11 +24,11 @@ string getLastLine(fstream file) {
 			file.seekg(0);
 			keep_looping = false;
 		}
-		else if (ch == "\n") {
+		else if (ch == '\n') {
 			keep_looping = false;
 		}
 		else {
-			file.seekg(-2,ios_base::cur)
+			file.seekg(-2,std::ios_base::cur);
 		}
 	}
 	std::string last_line;
@@ -39,14 +36,31 @@ string getLastLine(fstream file) {
 	return last_line;
 }
 
-void scanUWB(XeThru::Uwb * uwb_) {
+int parseVitals(std::string last_line) {
+	std::string delimiter = "V:";
+	size_t idx = last_line.find(delimiter) + delimiter.length();
+	char c = last_line.at(idx);
+	return c-'0';
+}
+
+float parseDistance(std::string last_line) {
+	std::string delimiter = "D:";
+	return std::stof(last_line.substr(last_line.find(delimiter) + delimiter.length(), std::string::npos));
+}
+
+void scanUWB(float servo_angle) {
 	// Copied from main() function in uwb_main.cpp
 	// 6 second wait when you try to get uwb data
 	std::cout << "Waiting 12 seconds...\n";
-	sleep(1000);
+	sleep(1);
 	// Read from last line of file
-	file.open("test.txt"); // Change this to correct filename
-	string last_line = getLastLine(file);
+
+	std::fstream file;
+	file.open("/home/parallels/catkin_ws/src/finderbot/src/test.txt"); // Change this to correct filename
+	if (file.fail()) std::cout << "FILE FAILED\n";
+	else std::cout << "FILE SUCCEEDED\n";
+	std::string last_line = getLastLine(file);
+	std::cerr << "LAST LINE: " << last_line << "\n";
 	file.close();
 	
 	int uwb_vital = parseVitals(last_line);
@@ -56,22 +70,13 @@ void scanUWB(XeThru::Uwb * uwb_) {
 	//  and distance to vitals (<=0 if not found)
 	uwb_data.vital = uwb_vital;
 	uwb_data.distance = uwb_distance;
+	uwb_data.angle = servo_angle;
 	uwb_publisher.publish(uwb_data);
-	ROS_INFO("Just published [ Person Detected: %zd, Distance: %zd ]",
+	ROS_INFO("Just published [ Person Detected: %i, Distance: %f ]",
 			 uwb_data.vital, uwb_data.distance);
 	ros::spinOnce();
 
 	return;
-}
-
-int parseVitals(string last_line) {
-	string delimiter = "V:";
-	return std::atoi(last_line.at(last_line.find(delimiter) + delimiter.length()));
-}
-
-float parseDistance(string last_line) {
-	string delimiter = "D:";
-	return std::stof(last_line.substr(last_line.find(delimiter) + delimiter.length(), std::string::npos));
 }
 
 // V:i D:f \n
@@ -79,42 +84,42 @@ float parseDistance(string last_line) {
 bool rotateAndScan() {
 
 	// Servo init stuff copied from servo.c
-	servo_setup();
+	// servo_setup();
 
 	// response.person_found = 0; // worry about this later
 
 	// 1. Rotate the servo to 0 degrees (absolute-90)
 	// 		** make sure it blocks the scan function
-	servo_turn0();
+	// servo_turn0();
 	// 2. Once it finishes rotating, scan new uwb data
 	// 		use functions as in uwb_main.cpp
 	// 		uwb_->state says if there is a person
 	// 		block task 3 until you have finished scanning
-	scanUWB();
+	scanUWB(0);
 	// 3. Rotate the servo to 90 degrees (absolute)
 	// 		** make sure it blocks the scan function (series not parallel)
-	servo_turn90();
+	// servo_turn90();
 	// 4. Once it finishes rotating, scan new uwb data
 	// 		use functions as in uwb_main.cpp
 	// 		uwb_->state says if there is a person
 	// 		block task 3 until you have finished scanning
-	scanUWB();
+	scanUWB(90);
 	// 5. Rotate the servo to 180 degrees (absolute+90)
 	// 		** make sure it blocks the scan function (series not parallel)
-	servo_turn180();
+	// servo_turn180();
 	// 6. Once it finishes rotating, scan new uwb data
 	// 		use functions as in uwb_main.cpp
 	// 		uwb_->state says if there is a person
 	// 		block task 3 until you have finished scanning
-	scanUWB();
+	scanUWB(180);
 
 	// Middle again
-	servo_turn90();
-	scanUWB();
+	// servo_turn90();
+	scanUWB(90);
 
 	// Left again
-	servo_turn0();
-	scanUWB();
+	// servo_turn0();
+	scanUWB(0);
 
 	return true;
 }
@@ -124,6 +129,8 @@ int main(int argc, char** argv)
 	// 0. Init
 	// 		Get absolute current row, col, theta
     ros::init(argc, argv, "UWB_Scan");
+	ros::NodeHandle nh;
+	ros::Publisher uwb_publisher = nh.advertise<finderbot::UWB_data>("/UWB_data", 1, true);
 
 	// ros::ServiceServer service = nh.advertiseService("UWB_Scan", rotateAndScan);
     std::cout << "Ready to scan UWB for people." << std::endl;
